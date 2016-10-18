ecdc2de849a50ca4b8ea9bf698d14e7e16b6a20d
rotateRp <1360617797@qq.com>
2016-10-18 15:00:33 +0800

    index js log

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function(){
 	console.log(111111111111111111111111111111111111111);
 	console.log(111111111111111111111111111111111111111);
 }
+function(){
+	
+	console.log(22);	
+	console.log(33);
+	console.log(44);
+	console.log(55);
+	
+
+}
